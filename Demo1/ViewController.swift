20ad02e834afe95a2af45b0d2953f6a1d745ef0c
Igor Kislyuk <glyerkg@gmail.com>
2023-01-23 22:45:37 +0700

    Initial commit

---
+++ Demo1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Demo1
+//
+//  Created by Igor Kislyuk on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        let button = UIButton(frame: .zero)
+        button.setTitle("Purchase", for: .normal)
+        view.addSubview(button)
+        button.setTitleColor(.blue, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        view.updateConstraintsIfNeeded()
+    }
+}
